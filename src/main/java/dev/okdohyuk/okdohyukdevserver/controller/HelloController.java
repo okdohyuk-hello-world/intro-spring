1fcc36d83336354b0bf3d5ef9db37eea03501e61
Dohyuk You <root@okdohyuk.dev>
2023-05-19 14:32:39 +0000

    feat: html templates

---
+++ src/main/java/dev/okdohyuk/okdohyukdevserver/controller/HelloController.java
@@ -0,0 +1,16 @@
+package dev.okdohyuk.okdohyukdevserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello");
+        return "hello";
+    }
+    
+}
