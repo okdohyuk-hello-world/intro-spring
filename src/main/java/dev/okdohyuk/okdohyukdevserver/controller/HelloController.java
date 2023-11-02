80c9045aa0fee17ede0c59a05fffc2021191779a
Dohyuk You <root@okdohyuk.dev>
2023-11-02 14:13:36 +0000

    [feat] hello mvc

--- src/main/java/dev/okdohyuk/okdohyukdevserver/controller/HelloController.java
+++ src/main/java/dev/okdohyuk/okdohyukdevserver/controller/HelloController.java
@@ -1,6 +1,7 @@
-package dev.okdohyuk.okdohyukdevserver;
+package dev.okdohyuk.okdohyukdevserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 
@@ -13,4 +14,9 @@ public class HelloController {
         return "hello";
     }
     
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 }
