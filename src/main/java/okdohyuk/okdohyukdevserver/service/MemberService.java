898481ec5cdaf3c097d2307d642cd85c1a24ae30
Dohyuk You <root@okdohyuk.dev>
2023-11-04 04:19:30 +0000

    [feat] member service

---
+++ src/main/java/okdohyuk/okdohyukdevserver/service/MemberService.java
@@ -0,0 +1,35 @@
+package okdohyuk.okdohyukdevserver.service;
+
+import java.util.List;
+import okdohyuk.okdohyukdevserver.domain.Member;
+import okdohyuk.okdohyukdevserver.repository.MemberRepository;
+import okdohyuk.okdohyukdevserver.repository.MemoryMemberRepository;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /** 회원가입 */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /** 전체 회원 조회 */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /** 회원 조회 */
+    public Member findOne(Long memberId) {
+        return memberRepository.findById(memberId).orElse(null);
+    }
+}
