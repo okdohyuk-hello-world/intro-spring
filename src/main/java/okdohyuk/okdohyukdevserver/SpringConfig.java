70610991df9ab125d3957075bcccf3a34ec73fa8
Dohyuk You <root@okdohyuk.dev>
2023-11-05 01:24:41 +0000

    [feat] jdbc member repository

--- src/main/java/okdohyuk/okdohyukdevserver/SpringConfig.java
+++ src/main/java/okdohyuk/okdohyukdevserver/SpringConfig.java
@@ -1,14 +1,26 @@
 package okdohyuk.okdohyukdevserver;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import okdohyuk.okdohyukdevserver.repository.JdbcMemberRepository;
 import okdohyuk.okdohyukdevserver.repository.MemberRepository;
-import okdohyuk.okdohyukdevserver.repository.MemoryMemberRepository;
+// import okdohyuk.okdohyukdevserver.repository.MemoryMemberRepository;
 import okdohyuk.okdohyukdevserver.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
